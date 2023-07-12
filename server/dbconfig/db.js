89f2982a1db3498b5eeae3e1d66f0ab6192312b2
Rahul Ohol <rahulohol01@gmail.com>
2023-07-12 23:49:46 +0530

    removed mongo url

--- server/dbconfig/db.js
+++ server/dbconfig/db.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const db =
-  "mongodb+srv://rahulohol:rahulohol@cluster0.qyig40b.mongodb.net/authusers?retryWrites=true&w=majority";
-
 const connection = mongoose.connect(process.env.mongo_url, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
